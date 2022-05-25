19a0ded9a4715b8312d1c819c0301ee36166edba
Nicole Koulov <nkk170000@europa.circ.utdallas.edu>
2022-05-25 13:07:52 -0500

    modified jobgen.py

--- tvb_slurm/jobgen.py
+++ tvb_slurm/jobgen.py
@@ -1,16 +1,16 @@
-with open('/scratch/yxw190015/github/tvbdemos/tvb_slurm/Gc.txt') as f:
+with open('/scratch/nkk170000/tvbdemos/tvb_slurm/Go.txt') as f:
     gc = f.read().splitlines()
 f.close()
 
-with open('/scratch/yxw190015/github/tvbdemos/tvb_slurm/group.txt') as f:
+with open('/scratch/nkk170000/tvbdemos/tvb_slurm/group.txt') as f:
     group = f.read().splitlines()
 f.close()
 
-with open('/scratch/yxw190015/github/tvbdemos/tvb_slurm/caseid.txt') as f:
+with open('/scratch/nkk170000/tvbdemos/tvb_slurm/caseid.txt') as f:
     caseid = f.read().splitlines()
 f.close()
 
 
-with open('/scratch/yxw190015/github/tvbdemos/tvb_slurm/jobfile', 'w') as f:
+with open('/scratch/nkk170000/tvbdemos/tvb_slurm/jobfile', 'w') as f:
     for i in range(73):
         print(f"python sim2hdf5.py --grp '{group[i]}' --caseid '{caseid[i]}' --gc '{gc[i]}' --time '180000'", file=f)
