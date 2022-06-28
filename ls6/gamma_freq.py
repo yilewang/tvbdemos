6cf2d73068b1100e59b4fc4b79c466680d0aa6e9
yile <ylwwayne@gmail.com>
2022-06-28 12:44:24 -0500

    nicole

--- ls6/gamma_freq.py
+++ ls6/gamma_freq.py
@@ -6,8 +6,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 from scipy.signal import hilbert, chirp
-#sys.path.append("/mnt/w/github/tvbtools")
-from tools.signaltools import SignalToolkit
+sys.path.append("HERE") # **put your tvbtools path inside
+from tvbtools.signaltools import SignalToolkit
 import ipywidgets as widgets
 my_layout = widgets.Layout()
 plt.style.use('ggplot')
@@ -23,8 +23,10 @@ with open('/work/08008/yilewang/tvbsim3mins/caseid.txt') as f:
 f.close()
 
 
+
+
 # import example data
-# filename = "/mnt/c/Users/wayne/tvb/gc3mins/SNC/2820A.h5"
+
 filename_path = "/work/08008/yilewang/tvbsim3mins/gc3mins/"
 
 parser = argparse.ArgumentParser(description='pass a float')
@@ -32,9 +34,11 @@ parser.add_argument('--path',type=str, required=True, help='the path we put our 
 args = parser.parse_args()
 
 
+
 if __name__ == "__main__":
-    subjectdf = pd.DataFrame(columns=["group", "caseid", "freq", "amp", "delay"])
+    subjectdf = pd.DataFrame(columns=["group", "caseid", "freq_gamma_left", "freq_gamma_right", "freq_theta_left", "freq_theta_right"])
     for one, two in zip(group, caseid):
+        print(one, two)
         ############## generate individual filename 
         filename = filename_path + one +"/"+ two+".h5"
         # create an instance
@@ -48,22 +52,17 @@ if __name__ == "__main__":
         spikesparas_af= {'prominence': 0.5, 'width':3000, 'height': 0.}
 
         # to generate raw and filtered data, including the spikes and valleys
-        pcg_left = subject.signal_package(dset, 4, 'pcg_left', True, spikesparas, valleysparas,spikesparas_af)
-        pcg_right = subject.signal_package(dset, 5, 'pcg_right', True, spikesparas, valleysparas,spikesparas_af)
+        pcg_left = subject.signal_package(dset, 4, 'pcg_left', 2.0, 10.0, True, spikesparas, valleysparas,spikesparas_af)
+        pcg_right = subject.signal_package(dset, 5, 'pcg_right', 2.0, 10.0, True, spikesparas, valleysparas,spikesparas_af)
 
-        # visualization of two signals
-        # subject.signal_af(**pcg_left)
-        # subject.signal_af(**pcg_right)
 
         # to calculate the frequency
-        freq_num = subject.freq_count(spikeslist=pcg_left["spikeslist"])
+        freq_gamma_left = subject.freq_count(spikeslist=pcg_left["spikeslist"])
+        freq_theta_left = subject.freq_count(spikeslist=pcg_left["spikeslist_af"])
+        freq_gamma_right = subject.freq_count(spikeslist=pcg_right["spikeslist"])
+        freq_theta_right = subject.freq_count(spikeslist=pcg_right["spikeslist_af"])
 
-        # to calculate the amplitude
-        amp_dergee = subject.amp_count(data=pcg_left["data"], spikeslist=pcg_left['spikeslist'], valleyslist=pcg_left["valleyslist"], mode="p2v")
-
-        # to calculate the delay between 2 signals
-        delay_time = subject.phase_delay(data1=pcg_left["data"], data2=pcg_right["data"], spikeslist1=pcg_left["spikeslist"], spikeslist2=pcg_right["spikeslist"], valleyslist1=pcg_left['valleyslist'], valleyslist2=pcg_right["valleyslist"], mode = "SI")
-
-        subjectdf = pd.concat([subjectdf, pd.DataFrame.from_records([{"group": one, "caseid":two, "freq":freq_num, "amp":amp_dergee, "delay":delay_time}])], ignore_index = True)
+        # write into DataFrame
+        subjectdf = pd.concat([subjectdf, pd.DataFrame.from_records([{"group": one, "caseid":two, "freq_gamma_left":freq_gamma_left, "freq_gamma_right":freq_gamma_right, "freq_theta_left":freq_theta_left, "freq_theta_right":freq_theta_right}])], ignore_index = True)
         subjectdf.to_excel(args.path)
         print("done")
