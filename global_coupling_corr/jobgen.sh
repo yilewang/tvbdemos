a74643366204ed33130a71c713918a083893b10c
wsl2 Yile Wang <yile.wang@utdallas.edu>
2023-02-18 19:19:29 -0600

    change g to 0.01 stepsize

--- global_coupling_corr/jobgen.sh
+++ global_coupling_corr/jobgen.sh
@@ -5,7 +5,7 @@
 
 while IFS=$' \t\r\n' read -r group && IFS= read -r caseid <&3;
 do
-    for j in $(seq 0.01 0.001 0.07);
+    for j in $(seq 0.01 0.01 0.07);
     do
         echo -e "python --G $j --Group $group --Caseid $caseid" >> jobfile
     done
