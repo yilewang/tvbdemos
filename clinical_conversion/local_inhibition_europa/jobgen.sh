08e961249989fdc8bf5a26f481e5498d1330c81a
Yat-Lok Wong <ylwwayne@gmail.com>
2023-09-15 17:35:14 -0500

    update the local inhibition in Europa, the jobfile, launcher slurm and dataset

---
+++ clinical_conversion/local_inhibition_europa/jobgen.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+# group=$`cat group.txt`
+# caseid=$`cat caseid.txt`
+
+while IFS=$' \t\r\n' read -r group && IFS=$' \t\r\n' read -r caseid <&3 && IFS=$' \t\r\n' read -r go <&4;
+do
+    for j in $(seq 0.01 0.001 0.07);
+    do
+        echo "python K21_fitting.py --Group $group --Caseid $caseid --Go $go --K21 $j"  >> jobfile
+    done
+done < group.txt 3< caseid.txt 4< go.txt
+
