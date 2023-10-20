25bfaab3b9b9ac15b7f956351b5d6a05e5de987b
Yat-Lok Wong <ylwwayne@gmail.com>
2023-10-20 11:07:16 -0500

    update new simulation range 0.0 to 0.4

--- clinical_conversion/local_inhibition_fc/jobgen.sh
+++ clinical_conversion/local_inhibition_fc/jobgen.sh
@@ -5,7 +5,7 @@
 
 while IFS=$' \t\r\n' read -r group && IFS=$' \t\r\n' read -r caseid <&3 && IFS=$' \t\r\n' read -r go <&4;
 do
-    for j in $(seq 0.0 0.01 0.4);
+    for j in $(seq 0.1 0.01 0.4);
     do
         echo "python K21_fitting.py --Group $group --Caseid $caseid --Go $go --K21 $j"  >> jobfile
     done
