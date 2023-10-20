378f6b67cc06132040f5e9887b96cf1a18ce3e7b
Yat-Lok Wong <ylwwayne@gmail.com>
2023-10-20 10:54:58 -0500

    update 0.1 jobfiles

--- clinical_conversion/local_inhibition_fc/jobgen.sh
+++ clinical_conversion/local_inhibition_fc/jobgen.sh
@@ -5,7 +5,7 @@
 
 while IFS=$' \t\r\n' read -r group && IFS=$' \t\r\n' read -r caseid <&3 && IFS=$' \t\r\n' read -r go <&4;
 do
-    for j in $(seq 0.1 0.1 1.0);
+    for j in $(seq 0.0 0.01 1.0);
     do
         echo "python K21_fitting.py --Group $group --Caseid $caseid --Go $go --K21 $j"  >> jobfile
     done
